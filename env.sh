70be1bc7b5881eda95bfbe81590cd082024a275f
Lê Hoàng Anh <lehoanganh.le2001@gmail.com>
2026-04-10 16:25:38 +0700

    feat: add Docker support with build scripts and configuration

---
+++ env.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+for i in $(env | grep RUNTIME_)
+do
+    key=$(echo $i | cut -d '=' -f 1)
+    value=$(echo $i | cut -d '=' -f 2-)
+    echo $key=$value
+
+    find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
+done
